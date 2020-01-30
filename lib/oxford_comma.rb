3758fcf402216439717eb95e37f128a1b0b43921
Jack Madden <jck13mad@gmail.com>
2020-01-30 00:28:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  if array.length
+  if array.length > 1 && array.length < 2
+    new_string = array.join(" and ")
+    return new_string
+  elsif array.length > 2
+    brand_new = array.join(", ")
+  end
 end
